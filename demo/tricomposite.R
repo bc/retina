fea86902e1c80012c48dd3ac504b6fdd6e1a0713
Brian Cohn <bcohn12@users.noreply.github.com>
2015-11-17 16:38:50 -0800

    removed from beta into production

--- demo/tricomposite.R
+++ demo/tricomposite.R
@@ -1,5 +1,3 @@
-message('This is a beta feature and is on it\'s way toward relase.\n -Brian July 11, 2014')
-
 retina_list <- list(Pmol_752=Pmol_752,
 					Pmol_753=Pmol_753, 
 					Pmol_751=Pmol_752 )
