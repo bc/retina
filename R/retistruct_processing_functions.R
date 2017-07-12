8c220b0b630fdd4334fad2832a63c7528759572b
Brian Cohn <bcohn12@gmail.com>
2017-07-12 16:37:11 -0700

    working fn for creating annotated-outlines with manual tear files

--- R/retistruct_processing_functions.R
+++ R/retistruct_processing_functions.R
@@ -15,15 +15,33 @@ dss_retistruct_processing <- function(path) {
 ##' @title Set up tear matrix
 ##' @description
 ##' takes in a list of tear triplets (numeric vectors of 3 elements) and outputs the dataframe, ready for retistruct processing
+##' @param list_of_tear_triplets list of 3-element vectors
+##' @param outline_object See ?retistruct::labelTearPoints for documentation
 ##' @return mat matrix of tear vals, ready for T.csv saving
 ##' @author Brian Cohn \email{brian.cohn@@usc.edu}
 ##' @export
-compose_tear_triplets_dataframe <- function(list_of_tear_triplets) {
-	permute_tear_vertices <- function(list_of_tear_triplets){
+compose_tear_triplets_dataframe <- function(list_of_tear_triplets, outline_object) {
+	permute_tear_vertices <- function(list_of_tear_triplets,outline_object){
 		lapply(list_of_tear_triplets, function(x){
-		retistruct::labelTearPoints(annotated_outline_with_tears,x)
+		retistruct::labelTearPoints(outline_object,x)
 			})
 		}
 
-    	do.call('rbind',permute_tear_vertices(list_of_tear_triplets))
+    	do.call('rbind',permute_tear_vertices(list_of_tear_triplets, outline_object))
+}
+##' @title Set up tear matrix within the annotated outline object
+##' @description
+##' assembles the outline object with tear data at $V0, $VB, and $VF
+##' @param outline_object See ?retistruct::labelTearPoints for documentation
+##' @param tear_coordinates_dataframe dataframe with $V0, $VB, and $VF columns as produced by compose_tear_triplets_dataframe
+##' @return annotated_outline See ?retistruct::labelTearPoints for documentation
+##' @author Brian Cohn \email{brian.cohn@@usc.edu}
+##' @export
+update_outline_object_tears <- function(outline_object, tear_coordinates_dataframe){
+	outline_copy <- outline_object
+  browser()
+	outline_copy$V0 <- tear_coordinates_dataframe[,1]
+	outline_copy$VB <- tear_coordinates_dataframe[,2]
+	outline_copy$VF <- tear_coordinates_dataframe[,3]
+	return(outline_copy)
 }
