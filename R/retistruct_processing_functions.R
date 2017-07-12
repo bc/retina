4853094d93d58a8c917a38b6ff872cea5df56f7e
Brian Cohn <bcohn12@gmail.com>
2017-07-12 15:34:10 -0700

    added tear-matrix functions for composition

--- R/retistruct_processing_functions.R
+++ R/retistruct_processing_functions.R
@@ -11,3 +11,19 @@ dss_retistruct_processing <- function(path) {
     rad <- retistruct.reconstruct(rad, report=do_not_print, plot.3d=FALSE, )  ## Reconstruct (computation intensive)
     return(rad)
 }
+
+##' @title Set up tear matrix
+##' @description
+##' takes in a list of tear triplets (numeric vectors of 3 elements) and outputs the dataframe, ready for retistruct processing
+##' @return mat matrix of tear vals, ready for T.csv saving
+##' @author Brian Cohn \email{brian.cohn@@usc.edu}
+##' @export
+compose_tear_triplets_dataframe <- function(list_of_tear_triplets) {
+	permute_tear_vertices <- function(list_of_tear_triplets){
+		lapply(list_of_tear_triplets, function(x){
+		retistruct::labelTearPoints(annotated_outline_with_tears,x)
+			})
+		}
+
+    	do.call('rbind',permute_tear_vertices(list_of_tear_triplets))
+}
