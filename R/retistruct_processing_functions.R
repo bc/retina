ec8c1a07ab1a2d453dc2832f72d2eb4e9d3b47b3
Brian Cohn <bcohn12@gmail.com>
2017-07-11 22:09:12 -0700

    rm print messages from retistruct

--- R/retistruct_processing_functions.R
+++ R/retistruct_processing_functions.R
@@ -6,7 +6,8 @@
 ##' @author Brian Cohn \email{brian.cohn@@usc.edu}
 ##' @references Sterratt et. al. 2013
 dss_retistruct_processing <- function(path) {
+    do_not_print <- function(string){}
     rad <- retistruct.read.markup(retistruct.read.dataset(path))
-    rad <- retistruct.reconstruct(rad)  ## Reconstruct (computation intensive)
+    rad <- retistruct.reconstruct(rad, report=do_not_print, plot.3d=FALSE, )  ## Reconstruct (computation intensive)
     return(rad)
 }
